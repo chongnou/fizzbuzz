ddac0a5d1c7260b6ac1dafacc727c1c177f6d761
Chong Vang <vangcguy@gmail.com>
2018-05-14 09:46:09 -0500

    udpate

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,21 @@
+
+
+// print all numbers from 1-100
 for(let i = 1; i <= 100; i++){
-	if(i % 3 === 0){
+	if((i % 3 === 0) && (i % 5 === 0)){
+		console.log('fizzbuzz');
+		}
+	else if (i % 3 === 0){
 		console.log('fizz');
 		}
 	else if(i % 5 === 0){
 		console.log('buzz');
 		}
-
-	else if(i % 3 && i % 5 === 0){
-		console.log('fizzbuzz');
-		}
 	else {
 		console.log(i);
 		}
 	}
+
+// if number is divisible by 3, print 'fizz'
+// if number is divisible by 5, print 'buzz'
+// if number is divisible by 3, print 'fizzbuzz'
